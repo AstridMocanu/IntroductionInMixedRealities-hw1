071c667d506a95ac74f2d84252224045a18fce21
AstridMocanu <62291817+AstridMocanu@users.noreply.github.com>
2020-10-03 19:47:14 +0300

    Initial commit

---
+++ Assets/fall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fall : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+    int count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        count = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        count++;
+
+
+        if(count>400 && Input.touchCount>0)
+            rigidBody.useGravity = true;
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# IntroductionInMixedRealities
+ 
